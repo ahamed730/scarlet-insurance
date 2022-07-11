dfae8a4692a873dace20556ef076ce93b47d444e
Ahmed Hamed <ahamed@li-6a31b6cc-2e6d-11b2-a85c-904fbabf1421.ibm.com>
2022-07-11 14:45:50 -0400

    load database

--- scarlet_insurance/load_doctors.py
+++ scarlet_insurance/load_doctors.py
@@ -16,16 +16,20 @@ from homepage.models import Doctor, DoctorInformation, Patient
 
 
 fake = Faker()
-locations = ['NewYork, NY', 'Jersey City, NJ', 'Brooklyn, NJ']
+locations = ['New York, NY', 'Jersey City, NJ', 'Brooklyn, NY']
 specialties = ['Cardiology', 'Neurology', 'Pathology', 'Ophthalmology', 'Internal Medicine', 'Dermatology']
 
-for i in range(100):
+for i in range(50):
     first_name, last_name = fake.name().split()[0], fake.name().split()[1]
     phone_number = fake.phone_number()
     location = random.choice(locations)
     specialty = random.choice(specialties)
     email = f"{first_name}.{last_name}@ibmedical.com"
+    username = first_name[0]+last_name
     password = make_password('ibm1234')
+    Doctor.objects.create(username=username, password=password, first_name=first_name, last_name=last_name, email=email, phone_number=phone_number)
+    doctor = Doctor.objects.get(email=email)
+    DoctorInformation.objects.create(doctor=doctor, location=location, specialty=specialty)
 
 
     
